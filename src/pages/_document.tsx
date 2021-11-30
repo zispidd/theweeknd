9f2fafbd335c1ee11eaf902d386c71a6179c9d16
zispidd <zispidd@gmail.com>
2021-11-30 23:13:38 +0200

    added google analytics

---
+++ src/pages/_document.tsx
@@ -0,0 +1,59 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+          originalRenderPage({
+            enhanceApp: (App: any) => (props: any) =>
+              sheet.collectStyles(<App {...props} />),
+          })
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=G-4L16XPF4GD'
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4L16XPF4GD', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
